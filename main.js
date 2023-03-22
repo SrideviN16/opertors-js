de0318a808b39dfd55f48103592c31475b18b8de
SrideviN16 <sridevi16devi97@gmail.com>
2023-03-22 13:48:57 +0530

    update

---
+++ main.js
@@ -0,0 +1,137 @@
+     
+	 // variable declartion 
+	
+	 var a=7;
+	 typeof(a);
+     console.log(typeof(a));	 
+	 var b="4";
+	 typeof(b);
+	 console.log(typeof(b));
+	 var c='hai'
+	 typeof(c);
+	 console.log(typeof(c));
+	 var a50=null;
+	 console.log(a50);
+	 var a51;
+	 console.log(a51);
+	 
+	
+	 //maths function
+	 // add//
+	 var a1=10;
+	 var a2=20;
+	 console.log(a1+a2);
+	 //sub//
+	 var a3=4;
+	 var a4=2;
+	 console.log(a3-a4);
+	  //multi//
+	 var a5=20;
+	 var a6=20;
+	 console.log(a5*a6);
+	  //div//
+	 var a7=10;
+	 var a8=2;
+	 console.log(a7/a8);
+	  //modul//
+	 var a9=40;
+	 var a10=3;
+	 console.log(a9%a10);
+	  //pre increment//
+	  var a11=26;
+	  console.log(++a11);
+	 //post increment//
+	  var a12=25;
+	  console.log(a12++);
+	  console.log(a12);
+	   //pre decrement//
+	  var a13=60;
+	  console.log(--a13);
+	 //post increment//
+	  var a14=25;
+	  console.log(a14--);
+	  console.log(a14);
+	  //greater
+	  var a15=60;
+	  var a16=62;
+	  console.log(a16>a15);
+	  //less than
+	  var a15=60;
+	  var a16=62;
+	  console.log(a16<a15);
+	  //greater than equal
+	  var a17=60;
+	  var a18=62;
+	  console.log(a17>=a18);
+	  //less than equal
+	  var a17=60;
+	  var a18=62;
+	  console.log(a17<=a18);
+	 //equal
+	  var a19=60;
+	  var a20=60;
+	  console.log(a19==a20);
+	   //double equal
+	  var a21=60;
+	  var a22='60';
+	  console.log(a21===a22);
+	  // not equal 
+	  var a23=34;
+	  var a24=39;
+	  console.log(a23!=a24);
+	  //double not equal 
+	  var a25=40;
+	  var a26='40';
+	  console.log(a25!==a26);
+	  //name compar//
+	  var name1="hai";
+	  var name2="hello";
+	  console.log(name1>name2);
+	  //expnt //
+	  var e=20;
+	  var f=2;
+	  console.log(e**f);
+	  // logical opertaor //
+	  //AND operator //
+	  var b=3;
+	  var b2=5;
+	  var b3=9;
+	  console.log((b>b2)&&(b3>b2));
+	  console.log((b3>b2)&&(b2>b));
+	  // or operator //
+	  var b4=6;
+	  var b5=8;
+	  var b6=100;
+	  console.log((b5>b4)||(b5>b6));
+	  console.log((b4>b6)||(b5>b4));
+	  //not operator//
+	 //assignment operator//
+	  var b7=5;
+	  console.log(b7+=6);
+	  var b8=4;
+	  console.log(b8-=3);
+	  var b9=2;
+	  console.log(b9*=3);
+	  var b10=5;
+	  console.log(b10%5);
+	  var b11=5;
+	  console.log(b11/5);
+	  // use string in assignment operator//
+	  var b12='4'
+	  console.log(b12+=3);
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	 
+	 
+	 
+	 
